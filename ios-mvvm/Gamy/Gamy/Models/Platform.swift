a37787006da3c906017b32f5986552b37e59a4d8
ariltw <ariltw@live.com>
2021-02-20 01:14:38 +0700

    Add base module

---
+++ ios-mvvm/Gamy/Gamy/Models/Platform.swift
@@ -0,0 +1,22 @@
+//
+//  Platform.swift
+//  Gamy
+//
+//  Created by Ariel on 04/11/20.
+//
+
+class Platform: Decodable {
+    var id: Int?
+    var name: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        name = try values.decodeIfPresent(String.self, forKey: .name)
+    }
+}
